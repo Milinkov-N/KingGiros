cec1ecc63fc1d06fe865e77de65bb4ce37d7562b
Milinkov-N <milinkov.nik@gmail.com>
2022-03-11 19:52:03 +0400

    completed header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import CartModal from '../CartModal'
 import Container from '../Container'
 import Logo from '../Logo'
 import NavModal from '../NavModal'
+import { FaPhoneAlt } from 'react-icons/fa'
 
 import styles from './Header.module.css'
 
@@ -10,16 +11,18 @@ export default function Header() {
     <>
       <header className={ styles.header }>
         <Container className={ styles.container }>
-          <h2 className={ styles.title }>
-            <span>Вкусная еда</span>
-            <span>И напитки</span>
-          </h2>
+          <div className={ styles.title }>
+            <div>
+              <h2>Вкусная еда</h2>
+              <h3>И напитки</h3>
+            </div>
+          </div>
           <Logo size={ 120 } />
           <div className={ styles.delivery }>
             <p>Доставка еды <span>Воткинск</span></p>
             <a href='tel:+79508389999' className={ styles.phone }>
               <div className={ styles.iconWrap }>
-                {/* <FontAwesomeIcon className={ styles.icon } icon={ faPhoneAlt }/> */}
+                <FaPhoneAlt className={ styles.icon }/>
               </div>
               <span>8 950 838 99 99</span>
             </a>
