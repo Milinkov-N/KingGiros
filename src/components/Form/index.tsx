af932ab1d5069db7e7534978b401768da10fdd4c
Milinkov-N <milinkov.nik@gmail.com>
2022-03-14 17:41:47 +0400

    created Form component

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,55 @@
+import { ComponentPropsWithRef } from 'react'
+import useClassName from 'src/hooks/useClassName'
+import styles from './From.module.css'
+
+export default function Form({
+  className,
+  onSubmit,
+  children,
+  ...rest
+}: ComponentPropsWithRef<'form'>) {
+  const classNames = useClassName([styles.form, className])
+  return (
+    <form
+      className={ classNames }
+      onSubmit= { onSubmit }
+      { ...rest }
+    >
+      { children }
+    </form>
+  )
+}
+
+export interface InputProps extends ComponentPropsWithRef<'input'> {
+  label: string
+}
+
+function Input({
+  className,
+  name,
+  placeholder,
+  type,
+  label,
+  required,
+  onSelect
+}: InputProps) {
+  const classNames = useClassName([styles.form, className])
+
+  return (
+    <div className={ classNames }>
+      <label className={ styles.label } htmlFor={ name }>
+        { label }
+      </label>
+      <input
+        className={ styles.input }
+        type={ type }
+        name={ name }
+        placeholder={ placeholder }
+        required={ required }
+        onSelect={ onSelect }
+      />
+    </div>
+  )
+}
+
+Form.Input = Input
