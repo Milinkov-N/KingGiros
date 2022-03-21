d1b0910b5087c73c1f1655f7c68df41a99984f51
Milinkov-N <milinkov.nik@gmail.com>
2022-03-21 18:07:13 +0400

    added margin to OpenCart button

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,35 @@
 import { combineReducers, createStore } from 'redux'
 import reducers from './reducers'
 
+// function saveToLocalStorage(state: object) {
+//   if (typeof window !== 'undefined') {
+//     try {
+//       const serializedState = JSON.stringify(state)
+//       localStorage.setItem('persistantState', serializedState)
+//     } catch (e) {
+//       console.warn(e)
+//     }
+//   } 
+// }
+
+// function loadFromLocalStorage() {
+//   if (typeof window !== 'undefined') {
+//     try {
+//       const serializedState = localStorage.getItem('persistantState')
+//       if (serializedState === null) return undefined
+
+//       return JSON.parse(serializedState)
+//     } catch (e) {
+//       console.warn(e)
+
+//       return undefined
+//     }
+//   }
+// }
+
 const rootReducer = combineReducers(reducers)
 const appStore = createStore(rootReducer)
+// appStore.subscribe(() => saveToLocalStorage(appStore.getState()))
 
 export default appStore
 
