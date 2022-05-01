fb82fb689b14549343a99cea6221f8e39ae749b8
Milinkov-N <milinkov.nik@gmail.com>
2022-05-01 14:26:11 +0400

    fixed bug with checkout form submittion

--- pages/checkout/index.tsx
+++ pages/checkout/index.tsx
@@ -4,6 +4,7 @@ import { Form } from 'src/components'
 import Layout, { Container } from 'src/components/layout'
 import { ContactInfo, OrderDetails, Error } from 'src/components/checkout'
 import { useTypedSelector, useCheckout, useActions } from 'src/hooks'
+import { ErrType } from 'src/hooks/useCheckout'
 
 import styles from 'styles/CheckoutPage.module.css'
 
@@ -16,27 +17,25 @@ export default function Checkout() {
   const [showError, setShowError] = useState(false)
   const [phoneIsIncorrect, setPhoneIsIncorrect] = useState(false)
 
-  const onFormSubmit = () => {
-    handleSubmit({
-      onSuccess: () => {
-        setOrderSubmitted(true)
-        router.push('/checkout/completed')
-        resetCart()
-        resetState()
-      },
-      onError: (errType) => {
-        if (errType === 'req') {
-          setShowError(true)
-        }
-        
-        if (errType === 'input') {
-          setPhoneIsIncorrect(true)
-        }
+  const submitSettings = {
+    onSuccess: () => {
+      setOrderSubmitted(true)
+      router.push('/checkout/completed')
+      resetCart()
+      resetState()
+    },
+    onError: (errType: ErrType) => {
+      if (errType === 'req') {
+        setShowError(true)
       }
-    })
+
+      if (errType === 'input') {
+        setPhoneIsIncorrect(true)
+      }
+    },
   }
 
-  useEffect(() =>{
+  useEffect(() => {
     if (cartState.items.length <= 0 && !orderSubmitted) {
       router.back()
     }
@@ -45,18 +44,10 @@ export default function Checkout() {
   return (
     <Layout title='Оформление заказа'>
       <Container>
-        <Error
-          show={ showError }
-          onClose={ () => setShowError(false) }
-        />
-        { cartState.items.length > 0 && (
-          <Form
-            className={ styles.form }
-            onSubmit={ onFormSubmit }
-          >
-            <ContactInfo
-              phoneIsCorrect={ phoneIsIncorrect }
-            />
+        <Error show={showError} onClose={() => setShowError(false)} />
+        {cartState.items.length > 0 && (
+          <Form className={styles.form} onSubmit={handleSubmit(submitSettings)}>
+            <ContactInfo phoneIsCorrect={phoneIsIncorrect} />
             <OrderDetails />
           </Form>
         )}
--- src/hooks/useCheckout.ts
+++ src/hooks/useCheckout.ts
@@ -5,34 +5,37 @@ import { CartState } from 'src/store/reducers/cart'
 import { UserState } from 'src/store/reducers/user'
 import { getCartItemsStr, newOrderMessage, sendBotMessage } from 'src/utils'
 
-export interface handleSubmitOptions {
+export interface handleSubmitState {
   userState: UserState
   cartState: CartState
 }
 
+export type ErrType = 'req' | 'input'
+
 export interface handleSubmitProps {
-  onSuccess: () => void,
-  onError: (errType: 'req' | 'input') => void
+  onSuccess: () => void
+  onError: (errType: ErrType) => void
 }
 
 export default function useCheckout() {
   const userState = useTypedSelector(state => state.userReducer)
   const cartState = useTypedSelector(state => state.cartReducer)
-  
+
   function handleSubmit({ onSuccess, onError }: handleSubmitProps) {
     return (e: FormEvent<HTMLFormElement>) => {
       e.preventDefault()
-  
+
       const orderDetails = getCartItemsStr(cartState.items)
       const text = newOrderMessage({ userState, cartState, orderDetails })
-      const chat_id = process.env.NODE_ENV !== 'development'
-        ? `${ process.env.NEXT_PUBLIC_TELEGRAM_KG_CHAT_ID }`
-        : `${ process.env.NEXT_PUBLIC_TELEGRAM_KG_DEV_CHAT_ID }`
-  
+      const chat_id =
+        process.env.NODE_ENV !== 'development'
+          ? `${process.env.NEXT_PUBLIC_TELEGRAM_KG_CHAT_ID}`
+          : `${process.env.NEXT_PUBLIC_TELEGRAM_KG_DEV_CHAT_ID}`
+
       async function fetchData() {
         try {
           const tgRes = await sendBotMessage(chat_id, text)
-          
+
           tgRes ? onSuccess() : onError('req')
         } catch (e) {
           console.error(e)
@@ -40,9 +43,7 @@ export default function useCheckout() {
         }
       }
 
-      userState.phone.length < 18
-        ? onError('input')
-        : fetchData()
+      userState.phone.length < 18 ? onError('input') : fetchData()
     }
   }
 
