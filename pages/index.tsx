89aaada607e62b54c418129775ad2c7662d803b5
Milinkov Nikita <73058993+Milinkov-N@users.noreply.github.com>
2023-07-10 12:06:31 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,7 @@ const query = `
           images(first: 1) {
             edges {
               node {
-                transformedSrc
+                url(transform: {scale: 1})
                 altText
               }
             }
